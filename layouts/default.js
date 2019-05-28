93475c9ec09f349b8480755a837b842069eb67d8
chips <chips@metrica.se>
2019-05-28 21:58:38 +0200

    Top nav

--- layouts/default.js
+++ layouts/default.js
@@ -1,9 +1,64 @@
 import Flex from 'kana-ui/dist/core/Flex'
+import Button from 'kana-ui/dist/core/Button'
+import Dropdown from 'kana-ui/dist/core/Dropdown'
+import {
+  Bars,
+  Home,
+  LayerGroup,
+  Wifi,
+  User,
+  Users,
+  Trash,
+} from 'styled-icons/fa-solid'
 
 export default ({ children, ...rest }) => {
+  const iconSize = 16
+  const UserMenu = (
+    <Flex>
+      <Button>
+        <Trash size={iconSize} />
+        Delete
+      </Button>
+      <Button>-profile</Button>
+      <Button>-subscription</Button>
+      <Button>
+        <Users size={iconSize} /># teams
+      </Button>
+      <Button>-team1</Button>
+      <Button>-team2</Button>
+      <Button>-personal</Button>
+      <Button>-manage teams</Button>
+      <Button># logout</Button>
+    </Flex>
+  )
   return (
     <>
-      <Flex wrap>xxx</Flex>
+      <Flex wrap>
+        <Button to="/">
+          <Bars size={iconSize} />
+        </Button>
+        <Button to="/">
+          <Home size={iconSize} />
+        </Button>
+        <Button to="/domains">
+          <LayerGroup size={iconSize} />
+          example.com
+        </Button>
+        <div style={{ flex: 1 }}>
+          <input
+            style={{ width: '100%' }}
+            placeholder="Search or type a /command here, try /new node"
+          />
+        </div>
+        <Button>
+          <Wifi size={iconSize} />
+        </Button>
+        <Dropdown align="right" content={UserMenu}>
+          <Button>
+            <User size={iconSize} />
+          </Button>
+        </Dropdown>
+      </Flex>
       <Flex>{children}</Flex>
     </>
   )
